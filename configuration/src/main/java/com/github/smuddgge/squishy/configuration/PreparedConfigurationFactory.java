45b76863336b0fcded84378809f7ffb466c00e34
Smudge <68658429+smuddgge@users.noreply.github.com>
2024-07-21 00:00:30 +0100

    Added prepared configuration factory

---
+++ configuration/src/main/java/com/github/smuddgge/squishy/configuration/PreparedConfigurationFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Java configuration and database library.
+ * Copyright (C) 2024  Smuddgge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.smuddgge.squishy.configuration;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class PreparedConfigurationFactory {
+
+    private final @NotNull ConfigurationFactory factory;
+    private final @NotNull File file;
+
+    public PreparedConfigurationFactory(@NotNull ConfigurationFactory factory, @NotNull File file) {
+        this.factory = factory;
+        this.file = file;
+    }
+
+    public @NotNull Configuration create() {
+        return this.factory.create(this.file);
+    }
+}
